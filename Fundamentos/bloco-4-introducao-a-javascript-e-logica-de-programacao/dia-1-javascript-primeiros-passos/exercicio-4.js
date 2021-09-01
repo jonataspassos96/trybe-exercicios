24b35567c7f3d54d19e3fa9c932035ee245ac9e4
Jonatas Passos <jpc080496@gmail.com>
2021-08-31 22:42:34 -0300

    Exercicios do 1 ao 8 finalizados

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-4.js
@@ -0,0 +1,9 @@
+const valor = 7;
+
+if (valor > 0) {
+    console.log("positivo");
+} else if (valor < 0) {
+    console.log("negativo");
+} else if (valor === 0) {
+    console.log("zero");
+}
---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5.js
@@ -0,0 +1,9 @@
+let a = 7;
+let b = 5;
+let c = 3;
+
+if (a+b>c && a+c>b && b+c>a) {
+    console.log(true);
+} else {
+    console.log(false);
+}
